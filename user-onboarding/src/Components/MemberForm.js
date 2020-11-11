361e85c2e79440c6e15f34c960b43a0f20adca1c
Adham AbdelFattah <adham.adel@gmail.com>
2020-11-11 16:30:08 -0600

    made pretty and started adding form logic

--- user-onboarding/src/Components/MemberForm.js
+++ user-onboarding/src/Components/MemberForm.js
@@ -1,30 +1,78 @@
 import React, { useEffect, useState } from 'react';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import Members from "./Members";
+import styled from "styled-components";
+import axios from "axios";
 
 
 export default function MemberForm () {
     const [members, setMembers] = useState([]);
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        agree: 'false',
+    });
 
     useEffect(()=> {
-        setMembers(['Adham'])
+        setMembers(['Adham', 'Wael', 'Mohamed'])
         },[])
 
 
     return(
         <>
+        <StyledFormParentDiv>
+        <h2> Join the Tantive team into hyperspace!</h2>
+        <StyledForm>
             <Form>
             <FormGroup>
-                    <Label for="exampleEmail">Email</Label>
-                    <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="examplePassword">Password</Label>
-                    <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
-                </FormGroup>
+                <Label for="name">Name</Label>
+                <Input type="name" name="email" id="name" placeholder="Name" />
+            </FormGroup>
+            <FormGroup>
+                <Label for="email">Email</Label>
+                <Input type="email" name="email" id="email" placeholder="Email" />
+            </FormGroup>
+            <FormGroup>
+                <Label for="password">Password</Label>
+                <Input type="password" name="password" id="password" placeholder="Password" />
+            </FormGroup>
+            {/* <br></br> */}
+            <FormGroup check>
+                <Label check>
+                <Input name= "accept" type="checkbox" />{' '}
+                    I accept the risks of space travel
+                </Label>
+            </FormGroup>
+            <br></br>
+            <Button color='primary'> Jump through hyperspace! </Button>
             </ Form>
-
-            <Members members={members}/>
+            
+        </StyledForm>
+        </ StyledFormParentDiv>
+        <Members members={members}/>
         </>
     )
 }
+
+const StyledFormParentDiv = styled.div `
+display: flex;
+flex-flow: column;
+align-items: center;
+justify-content: center;
+    h2{
+        padding: 25px;
+    }
+`
+
+const StyledForm = styled.div `
+/* border: solid blue 1px; */
+max-width: 90%;
+min-width: 40%;
+min-height: 10vh;
+display: flex;
+flex-flow: column;
+justify-content: space-around;
+align-items: center;
+`
+
--- user-onboarding/src/Components/Members.js
+++ user-onboarding/src/Components/Members.js
@@ -1,22 +1,40 @@
 import React from "react";
 import styled from "styled-components";
 import {Card, CardTitle, CardSubtitle} from "reactstrap";
-
+import {Button} from 'reactstrap';
 
 export default function Members (props) {
     const {members} = props;
 
 
     return (
-        <div>
+        <StyledMembersParentDiv>
+            <br></br>
             {members.map((member) => {
                 return (
-                    <Card>
-                        <CardTitle>{member}</CardTitle>
-                        <CardSubtitle>{member}</CardSubtitle>
-                    </Card>
+                    <StyledCard>
+                        <Card>
+                            <CardTitle>{member}</CardTitle>
+                            <CardSubtitle>{member}</CardSubtitle>
+                        </Card>
+                    </StyledCard>
                 )
             })}
-        </div>
+    
+        </StyledMembersParentDiv>
     );
 };
+
+
+const StyledMembersParentDiv = styled.div`
+/* border: blue solid 1px; */
+display: flex;
+flex-flow: column;
+
+align-items: center;
+`
+
+const StyledCard = styled.div`
+/* border: yellow solid 2px; */
+max-width: 70%;
+`
